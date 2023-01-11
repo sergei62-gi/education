9072ba4cc5870c394f319708a9674270637b9ff8
sergei62-gi <69719147+sergei62-gi@users.noreply.github.com>
2023-01-11 19:18:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 from string import ascii_uppercase
-namber = int(input(""))
-uppercase_litter = [i for i in ascii_uppercase]
-print(uppercase_litter[:namber])
+
+def main():
+    uppercase_litter = [i for i in ascii_uppercase]
+    return uppercase_litter
+
+def repetition_of_letters(a:list):
+    x = [a[i-1]*i for i in range(1, len(a)+1)]
+    return x
+
+def a_task():
+    phrase = 'Take only the words that start with t in this sentence'
+    list = [i for i in phrase.split() if i[0] == "t" or i[0] == "T"]
+    print(list)
+
+if __name__ == "__main__":
+    namber = int(input())
+    x = main()
+    print(x[:namber])
+
+    y = repetition_of_letters(x)
+    print(y[:namber])
+
+    a_task()
